eb1c30fc913b6487d6156c1d05e624e1d267b7ca
Pete <pete.chen@afusion.com>
2014-03-23 04:47:05 +0800

    add SeleniumDemo

---
+++ SeleniumDemo/SeleniumDemo.Tests/AccountTests.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumDemo.Tests
+{
+    [TestClass]
+    public class AccountTests
+    {
+        [TestMethod]
+        public void Can_Display_Register_Page()
+        {
+            string chromeDriverDirectory = string.Format(@"{0}\..\..\..\tools", Directory.GetCurrentDirectory());
+
+            IWebDriver driver = new ChromeDriver(chromeDriverDirectory);
+            driver.Navigate().GoToUrl("http://localhost:64872/");
+
+            IWebElement registerLink = driver.FindElement(By.Id("registerLink"));
+            registerLink.Click();
+
+            Assert.AreEqual("http://localhost:64872/Account/Register", driver.Url);
+
+            driver.Quit();
+        }
+    }
+}
