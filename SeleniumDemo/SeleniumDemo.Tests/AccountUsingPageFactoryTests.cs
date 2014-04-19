49d267d3308d1390ab297fbbecd330f43c61bf32
Pete <pete.chen@afusion.com>
2014-04-20 02:59:32 +0800

    add page factory

---
+++ SeleniumDemo/SeleniumDemo.Tests/AccountUsingPageFactoryTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumDemo.Tests
+{
+    [TestClass]
+    public class AccountUsingPageFactoryTests
+    {
+        private ChromeDriver _driver;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            string chromeDriverDirectory = string.Format(@"{0}\..\..\..\tools", Directory.GetCurrentDirectory());
+            this._driver = new ChromeDriver(chromeDriverDirectory);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            this._driver.Quit();
+        }
+
+        [TestMethod]
+        public void Can_Register_User()
+        {
+            RegisterPageUsingPageFactory registerPage = new RegisterPageUsingPageFactory(this._driver)
+            .Navigate()
+            .EnterUsername(string.Format("testuser{0}", DateTime.Now.Ticks))
+            .EnterPassword("P@ssw0rd")
+            .EnterConfirmPassword("P@ssw0rd");
+
+            HomePage homePage = registerPage.Submit();
+
+            Assert.IsNotNull(homePage);
+            Assert.IsTrue(homePage.LoggedIn);
+        }
+
+        [TestMethod]
+        public void Cannot_Register_User_With_Empty_Username()
+        {
+            RegisterPageUsingPageFactory registerPage = new RegisterPageUsingPageFactory(this._driver)
+            .Navigate()
+            .EnterUsername(string.Empty)
+            .EnterPassword("P@ssw0rd")
+            .EnterConfirmPassword("P@ssw0rd");
+
+            HomePage homePage = registerPage.Submit();
+
+            Assert.IsNull(homePage);
+            Assert.AreEqual("The User name field is required.", registerPage.ErrorMessage);
+        }
+    }
+}
---
+++ SeleniumDemo/SeleniumDemo.Tests/RegisterPageUsingPageFactory.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace SeleniumDemo.Tests
+{
+    public class RegisterPageUsingPageFactory
+    {
+        private IWebDriver _driver;
+        private string _username;
+
+#pragma warning disable 649
+
+        [FindsBy(How = How.Id, Using = "UserName")]
+        private IWebElement _usernameField;
+
+        [FindsBy(How = How.Id, Using = "Password")]
+        private IWebElement _passwordField;
+
+        [FindsBy(How = How.Id, Using = "ConfirmPassword")]
+        private IWebElement _confirmPassword;
+
+        [FindsBy(How = How.ClassName, Using = "btn")]
+        private IWebElement _registerButton;
+
+        [FindsBy(How = How.XPath, Using = "//div[@class='validation-summary-errors']/ul/li")]
+        private IList<IWebElement> _errorMessages;
+
+#pragma warning restore 649
+
+        public RegisterPageUsingPageFactory(IWebDriver driver)
+        {
+            this._driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        public RegisterPageUsingPageFactory Navigate()
+        {
+            this._driver.Navigate().GoToUrl("http://localhost:64872/Account/Register");
+            return this;
+        }
+
+        public RegisterPageUsingPageFactory EnterUsername(string username)
+        {
+            this._username = username;
+            this._usernameField.SendKeys(username);
+            return this;
+        }
+
+        public RegisterPageUsingPageFactory EnterPassword(string password)
+        {
+            this._passwordField.SendKeys(password);
+            return this;
+        }
+
+        public RegisterPageUsingPageFactory EnterConfirmPassword(string password)
+        {
+            this._confirmPassword.SendKeys(password);
+            return this;
+        }
+
+        public HomePage Submit()
+        {
+            this._registerButton.Click();
+
+            if (this._driver.Url.Contains("Register"))
+            {
+                return null;
+            }
+
+            HomePage homePage = new HomePage(this._driver);
+            return homePage;
+        }
+
+        public string ErrorMessage
+        {
+            get
+            {
+                IWebElement errorMessage = this._errorMessages.FirstOrDefault();
+                return errorMessage == null ? string.Empty : errorMessage.Text;
+            }
+        }
+    }
+}
