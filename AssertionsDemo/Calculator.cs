359359d80c5379369d65655a90c491078f93a645
Pete <pete.chen@afusion.com>
2014-06-14 11:11:04 +0800

    add demo for using fluent assertion

---
+++ AssertionsDemo/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AssertionsDemo
+{
+    public static class Calculator
+    {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int Subtract(int x, int y)
+        {
+            throw new NotSupportedException("Not yet implemented");
+        }
+    }
+}
---
+++ AssertionsDemo/CalculatorTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace AssertionsDemo
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void Can_Add_Integer()
+        {
+            int total = Calculator.Add(1, 2);
+            Assert.AreEqual(3, total);
+        }
+
+        [Test]
+        public void Can_Add_Integer_Using_Constraints()
+        {
+            int total = Calculator.Add(1, 2);
+            Assert.That(total, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Can_Add_Integer_Using_FluentAssertions()
+        {
+            int total = Calculator.Add(1, 2);
+            total.Should().Be(3);
+        }
+
+        [Test]
+        public void Cannot_Subtract_And_Throw_NotSupportedException()
+        {
+            Action action = () => Calculator.Subtract(5, 3);
+            action.ShouldThrow<NotSupportedException>().WithMessage("Not yet implemented");
+        }
+    }
+}
